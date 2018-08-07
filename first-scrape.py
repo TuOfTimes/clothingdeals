0d30045fff9c03f01d4482e18db733da7ad719fd
Edward <TuOfTimes@github.com>
2018-08-06 21:27:34 -0400

    followed web tutorial

--- first-scrape.py
+++ first-scrape.py
@@ -6,3 +6,31 @@ from selenium.common.exceptions import TimeoutException
 
 option = webdriver.ChromeOptions()
 option.add_argument(" - incognito")
+browser = webdriver.Chrome(executable_path='chromedriver', chrome_options=option)
+browser.get("https://www.abercrombie.com/shop/us/mens/?grid=true")
+
+# Wait 20 seconds for page to load
+timeout = 20
+try:
+    WebDriverWait(browser, timeout).until(EC.visibility_of_element_located((By.XPATH, "//img[@class='avatar width-full rounded-2']")))
+except TimeoutException:
+    print("Timed out waiting for page to load")
+    browser.quit()
+
+# find_elements_by_xpath returns an array of selenium objects.
+titles_element = browser.find_elements_by_xpath("//a[@class='text-bold']")
+# use list comprehension to get the actual repo titles and not the selenium objects.
+titles = [x.text for x in titles_element]
+# print out all the titles.
+print('titles:')
+print(titles, '\n')
+
+language_element = browser.find_elements_by_xpath("//p[@class='mb-0 f6 text-gray']")
+# same concept as for list-comprehension above.
+languages = [x.text for x in language_element]
+print("languages:")
+print(languages, '\n')
+
+for title, language in zip(titles, languages):
+    print("RepoName : Language")
+    print(title + ": " + language, '\n')
